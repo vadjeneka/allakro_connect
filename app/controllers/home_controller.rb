6f99c388f8bd8c0712df7a175ac2d7e5446955c7
kaudaouda <konedaouda7894@yahoo.com>
2021-11-22 16:55:04 +0000

    show home page methode

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,18 @@ class HomeController < ApplicationController
       @result = Product.search(params[:search])
     else
     end
+    @products = Product.all
+    
+  end
+
+  def products_rand
+    @articles = articles.all
+    offset = rand(100)
+    @products_rand = Product.offset(offset).limit(8)
+    
   end
 end
+
+
 
  
