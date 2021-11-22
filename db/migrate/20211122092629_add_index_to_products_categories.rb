245eb3f8bd14b7ba93132af5cd0252f90dc60523
Ibrahim Nouho <IbrahimNouho123@gmail.com>
2021-11-22 09:39:27 +0000

    saving

--- db/migrate/20211122092629_add_index_to_products_categories.rb
+++ db/migrate/20211122092629_add_index_to_products_categories.rb
@@ -1,5 +1,6 @@
-class AddIndexToProductsCategories < ActiveRecord::Migration[6.1]
+class AddIndexToCategoriesProducts < ActiveRecord::Migration[6.1]
   def change
-    add_index :products_categories, [:product_id, :category_id], unique: true
+    add_index :categories_products, [:product_id, :category_id], unique: true
+    add_index :categories_products, [:category_id, :product_id], unique: true
   end
 end
