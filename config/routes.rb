63c7f5bfb9f21f3321fe38a697dcfa25d686a314
Mylene Akaffou <myleneakaffou2@gmail.com>
2021-11-23 10:33:30 +0000

    authenticate with facebook

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,18 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "home#index"
+    devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    root to: "home#index"
 
-  if Rails.env.development?
-    mount Sidekiq::Web => '/sidekiq'
-  else
-    authenticate :user, lambda { |u| u.admin? } do
+    if Rails.env.development?
       mount Sidekiq::Web => '/sidekiq'
+    else
+      authenticate :user, lambda { |u| u.admin? } do
+        mount Sidekiq::Web => '/sidekiq'
+      end
     end
-  end
+
+   
+
 end
