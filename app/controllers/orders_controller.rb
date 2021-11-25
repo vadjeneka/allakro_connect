d6c6e32a8af6fd7bbf9c62e56496ed11debf490e
MarinaNguessan <marinanguessan23@gmail.com>
2021-11-25 10:51:52 +0000

    WIP: View orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,13 +1,16 @@
 class OrdersController < ApplicationController
 
   def index
-    @orders = current_user.orders
+    @orders = current_user.orders.includes(:cart)
   end
 
   def create
-    cart = params[:cart_id]
+    cart = Cart.find(params[:cart_id])
     @order = Order.new(user_id: current_user.id, cart_id: cart.id, amount: cart.total_price, is_fulfilled: false)
+
     if @order.save
+      # TODO: Set cart to validated: true
+      cart.update(validated: true)
       redirect_to user_orders_path(current_user), notice: 'Order was successfully created'
     else
       flash[:error] = "Couldn't create order"
