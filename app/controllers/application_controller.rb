f5337fa96233405a8bc1d9ee7edbcd2f17efb901
Djeneka <djeneka@192.168.1.29>
2021-11-29 17:00:44 +0000

    last

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,19 @@
 class ApplicationController < ActionController::Base
   include Pundit
+  protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def user_store_cart(user, store)
     @user_store_cart ||= find_or_create_cart(user, store)
   end
 
+  protected
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :phone, :town , :city, :password, :password_confirmation)}
+      devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:first_name, :last_name, :email, :phone, :town , :city, :password, :password_confirmation)}
+    end
+    
   private
   def find_or_create_cart(user, store)
     cart = Cart.find_by(user_id: user.id, store_id: store.id, validated: false)
--- app/models/account.rb
+++ app/models/account.rb
@@ -1,3 +1,17 @@
 class Account < ApplicationRecord
   belongs_to :user
+  
+  def deposit(amount)
+    @balance += amount if amount > 0
+    return 0
+  end
+
+  def withdraw(amount)
+    if amount <= @balance && amount > 0
+      @balance -= amount
+      return amount
+    end
+    return 0
+  end
+
 end
