c422c22a6715156ad745e4f3408e5e6b77a74d9b
Mylene Akaffou <myleneakaffou2@gmail.com>
2021-11-29 12:42:12 +0000

    authenticate Facebook and user registration done

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
-
+  before_action :authenticate_user, only: [:index]
   def index
+    user = User.find_by(email: current_user[:email])
+    #raise current_user.inspect
+    if user.first_name == nil && user.town ==nil && user.city == nil
+      redirect_to edit_user_registration_path(user)
+    end  
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
-    devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+    devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'registrations' }
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
     root to: "home#index"
 
