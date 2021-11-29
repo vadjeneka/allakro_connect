047216ca48ab9c1fc0bd0d058e13d6fd2c7f506a
Qirxk <michelkonan00@gmail.com>
2021-11-29 17:24:46 +0000

    saving...

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,7 @@
 class ProfilesController < ApplicationController
   before_action :authenticate_user!
   def show
+    @orders = Order.where(user_id: current_user.id)
     @favorites = Favorite.where(user_id: current_user.id, still_favorites?: true)
   end
 end
