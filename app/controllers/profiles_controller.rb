a18c9bba3c9e9e273ee4fb51e9f0a720054a70cd
Djeneka <djeneka@192.168.1.22>
2021-12-01 09:36:52 +0000

    account page with balance

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,6 +2,6 @@ class ProfilesController < ApplicationController
   before_action :authenticate_user!
   def show
     @favorites = Favorite.where(user_id: current_user.id, still_favorites?: true)
+    @account = Account.find_by(user_id: current_user.id) 
   end
-
 end
