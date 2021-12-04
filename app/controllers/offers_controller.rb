4f8d21d3594638f456d9fa4f7d664cd86ec550b8
stephane <andresstephane.as@gmail.com>
2021-12-04 20:27:49 +0000

    Design historic offer and bid with details okay

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -16,39 +16,39 @@ class OffersController < ApplicationController
     @offers = Offer.find_by(bid_id: params[:bid_id])
     # First offer
     
-    # if params_offer[:amount].to_i <= @bid.initial_price
-    #   flash[:error] = "Offre non enregistrée: elle doit être supérieure au prix initial"
-    #   redirect_to store_product_bid_path(@bid.product.store,@bid.product,@bid)
-    # elsif @offers == nil
-    #   @offer = @bid.offers.build(params_offer)
-    #   @offer.save
-    #   redirect_to store_product_bid_offers_path(@bid.product.store, @bid.product, @bid), notice: "Votre offre a été enregistrée"
-    # else
-    # # other offers
-    #   @offer = @bid.offers.build(params_offer)
-    #   if @offer.amount > @bid.offers.top.first.amount
-    #     @offer.save
-    #     redirect_to store_product_bid_offers_path(@bid.product.store, @bid.product, @bid), notice: "Votre offre a été enregistrée"
-    #   else
-    #     flash[:error] = "Offre non enregistrée: inferieure à l'offre gagnante"
-    #     redirect_to store_product_bid_path(@bid.product.store,@bid.product,@bid)
-    #   end
-    # end
-
-    if params_offer[:amount].to_i <= @bid.initial_price && @offers == nil
-      flash[:error] = "Offre non enregistrée: inferieure au prix initial"
+    if params_offer[:amount].to_i <= @bid.initial_price
+      flash[:error] = "Offre non enregistrée: elle doit être supérieure au prix initial"
       redirect_to store_product_bid_path(@bid.product.store,@bid.product,@bid)
-    
+    elsif @offers == nil
+      @offer = @bid.offers.build(params_offer)
+      @offer.save
+      redirect_to store_product_bid_offers_path(@bid.product.store, @bid.product, @bid), notice: "Votre offre a été enregistrée"
     else
-        @offer = @bid.offers.build(params_offer)
-        if @offer.amount > @bid.offers.top.first&.amount
-          @offer.save
-          redirect_to store_product_bid_offers_path(@bid.product.store, @bid.product, @bid), notice: "Votre offre a été enregistrée"
-        else
-          flash[:error] = "Offre non enregistrée: inferieure à l'offre gagnante"
-          redirect_to store_product_bid_path(@bid.product.store,@bid.product,@bid)
-        end
-    end 
+    # other offers
+      @offer = @bid.offers.build(params_offer)
+      if @offer.amount > @bid.offers.top.first.amount
+        @offer.save
+        redirect_to store_product_bid_offers_path(@bid.product.store, @bid.product, @bid), notice: "Votre offre a été enregistrée"
+      else
+        flash[:error] = "Offre non enregistrée: inferieure à l'offre gagnante"
+        redirect_to store_product_bid_path(@bid.product.store,@bid.product,@bid)
+      end
+    end
+
+    # if params_offer[:amount].to_i <= @bid.initial_price && @offers == nil
+    #   flash[:error] = "Offre non enregistrée: inferieure au prix initial"
+    #   redirect_to store_product_bid_path(@bid.product.store,@bid.product,@bid)
+    
+    # else
+    #     @offer = @bid.offers.build(params_offer)
+    #     if @offer.amount > @bid.offers.top.first.&.amount
+    #       @offer.save
+    #       redirect_to store_product_bid_offers_path(@bid.product.store, @bid.product, @bid), notice: "Votre offre a été enregistrée"
+    #     else
+    #       flash[:error] = "Offre non enregistrée: inferieure à l'offre gagnante"
+    #       redirect_to store_product_bid_path(@bid.product.store,@bid.product,@bid)
+    #     end
+    # end 
   end
 
   def historic
