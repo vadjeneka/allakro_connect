e6818f0c3604f5da0f883ba84f79ef909c84fb5a
Ibrahim Nouho <IbrahimNouho123@gmail.com>
2021-12-06 09:29:47 +0000

    saving

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,13 +1,3 @@
 class Stock < ApplicationRecord
-  belongs_to :
-  
-  def decrement_quantity(line_items)
-    @product = Product.find(params[:product_id])
-    @stock = @product.stock.quantity
-
-    if @product.stock != nil && line_items.quantity <= @product.stock.quantity
-      @stock = @stock - line_items.quantity
-    end 
-    
-  end
+  belongs_to: product
 end
