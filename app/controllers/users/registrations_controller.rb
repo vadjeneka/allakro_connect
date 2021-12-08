0c248fdd8ff1ff5aee49cb27f4d2d7ed66b33044
Qirxk <michelkonan00@gmail.com>
2021-12-08 12:03:21 +0000

    fix bugs

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -22,10 +22,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def after_sign_in_path_for(resource_or_scope)
     stored_location_for(resource_or_scope) || root_path
   end
-  
-  protected
-
-  def after_inactive_sign_up_path_for(resource)
+  def after_sign_up_path_for(resource)
     root_path
   end
 end
